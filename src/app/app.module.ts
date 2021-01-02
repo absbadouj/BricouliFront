a897fda5fdc4038b22aa1cab6ed86d1e2bb52aeb
Abdessalam BADOUJ <absbadouj@gmail.com>
2021-01-02 13:36:42 +0300

    First Commit 2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { CategorieComponent } from './categorie/categorie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccueilComponent,
+    SigninComponent,
+    SignupComponent,
+    CategorieComponent
   ],
   imports: [
     BrowserModule,
